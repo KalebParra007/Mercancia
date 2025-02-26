bd6de087266203a43de09a1b968ec7d6ca51dba4
Juan Camilo Medina <juancamilomedinafernandez2020@gmail.com>
2025-02-26 16:08:02 -0500

    Se desarrolla primer componente

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    return(
+        <section className="panel-control-card">
+                    <div></div>
+                    <div>
+                        <p>ver</p>
+                    </div>
+                </section>
+    )
+}
+
+export default Card
---
+++ src/PanelControl.jsx
@@ -0,0 +1,26 @@
+import imgUser from './assets/usuario.png'
+import Card from './Card'
+const PanelControl = () => {
+    return(
+        <section className="panel-control">
+            <aside className="panel-control-aside">
+                <div>
+                    <img src={imgUser} alt="logo" />
+                </div>
+                <nav>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </nav>
+            </aside>
+            <div className="panel-control-contenido">
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+        </section>
+    )
+}
+
+export default PanelControl
